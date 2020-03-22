ee0fca326b8e4bf713e4d5b9f42c4f8971769ec2
cacauisadog <cbcaue@gmail.com>
2020-03-21 21:16:40 -0300

    add mock removeTodo

--- src/helpers/apimock/everyone.js
+++ src/helpers/apimock/everyone.js
@@ -6,7 +6,15 @@ export default {
     return mockasync(todoList).then(response => response.data)
   },
   addNewTodo(newTodo) {
+    if (newTodo.text === 'kaboom') {
+      return mockasync({ error: 'addNewTodo() goes boom!' }).then(
+        response => response.data
+      )
+    }
     newTodo.id = Math.random() * 10
     return mockasync(newTodo).then(response => response.data)
+  },
+  removeTodo(todo) {
+    return mockasync(todo).then(response => response.data)
   }
 }
