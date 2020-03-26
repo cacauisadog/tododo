4c7ff545da2ca14c87356a4ce2be50b9a68eca49
cacauisadog <cbcaue@gmail.com>
2020-03-25 23:21:41 -0300

    hardcoded debug confs

--- api/tododo.py
+++ api/tododo.py
@@ -4,3 +4,7 @@ from app import api, db
 @api.shell_context_processor
 def make_shell_context():
     return {'db': db}
+
+
+if __name__ == '__main__':
+    api.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
