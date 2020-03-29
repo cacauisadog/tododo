d7753ae5c2b19aa59054fa8f4c334c8488cce7c4
cacauisadog <cbcaue@gmail.com>
2020-03-29 18:45:53 -0300

    refactor: jsonify is no longer needed in view

--- api/app/auth/routes.py
+++ api/app/auth/routes.py
@@ -6,29 +6,34 @@ from flask_login import current_user
 from app.auth import auth_svc, bp
 
 
+"""
+    As of flask v1.1, the `return` statement will automatically `jsonify` a dictionary in the first return value. So you 
+    can simply use `return dict, 200`. Note that 200 is the default status_code.
+    https://stackoverflow.com/questions/45412228/flask-sending-data-and-status-code-through-a-response-object
+"""
 @bp.route('/api/whoami', methods=['GET'])
 def whoami():
     if current_user.is_authenticated:
-        return jsonify(current_user.to_dict_json())
+        return current_user.to_dict_json()
 
-    return jsonify({'is_authenticated': False})
+    return {'is_authenticated': False}
 
 
 @bp.route('/api/signup', methods=['POST'])
 def signup():
     user = json.loads(request.form.get('user'))
     new_user = auth_svc.signup(user)
-    return jsonify(new_user)
+    return new_user
 
 
 @bp.route('/api/login', methods=['POST'])
 def login():
     if current_user.is_authenticated:
-        return jsonify(current_user)
+        return current_user
 
     user = json.loads(request.form.get('user'))
     user = auth_svc.login(user)
-    return jsonify(user)
+    return user
 
 
 @bp.route('/api/logout', methods=['POST'])
@@ -36,4 +41,4 @@ def logout():
     if current_user.is_authenticated:
         auth_svc.logout()
 
-    return jsonify({})
+    return {}
