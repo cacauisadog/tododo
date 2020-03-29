767f8366f8290901ea0280ef96cfc878e3133076
cacauisadog <cbcaue@gmail.com>
2020-03-29 18:47:10 -0300

    add better separation of dev and test configs

--- api/config.py
+++ api/config.py
@@ -1,18 +1,20 @@
 import os
 
 
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = os.getenv('DB_NAME', 'tododo')
+DB_USER = os.getenv('DB_USER', 'tododo')
+DB_PASS = os.getenv('DB_PASS', 'tododo')
+
+
 class Config(object):
     """Parent configuration class."""
     SECRET_KEY = os.getenv('SECRET_KEY', 'tododo')
     # APP_SETINGS = os.environ.get('APP_SETTINGS') or 'development'
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '5432')
-    DB_NAME = os.getenv('DB_NAME', 'tododo')
-    DB_USER = os.getenv('DB_USER', 'tododo')
-    DB_PASS = os.getenv('DB_PASS', 'tododo')
-    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(user=DB_USER, pw=DB_PASS, host=DB_HOST,
-                                                                           port=DB_PORT, db=DB_NAME)
-    SQLALCHEMY_DATABASE_URI = DB_URL
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(user=DB_USER, pw=DB_PASS,
+                                                                                            host=DB_HOST, port=DB_PORT,
+                                                                                            db=DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -24,7 +26,11 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configuration for testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://localhost/tododo_test'
+    TEST_DB_USER = 'test_tododo'
+    TEST_DB_NAME = 'test_tododo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(user=TEST_DB_USER, pw=DB_PASS,
+                                                                                            host=DB_HOST, port=DB_PORT,
+                                                                                            db=TEST_DB_NAME)
     DEBUG = True
 
 
